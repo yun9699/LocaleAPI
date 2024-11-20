212ef240ce1514837ffc81562656061937dce666
yun9699 <cha23215@daum.net>
2024-11-20 09:34:39 +0900

    build: SprongBoot Setting

---
+++ src/main/java/org/esnack24/localeapi/LocaleApiApplication.java
@@ -0,0 +1,13 @@
+package org.esnack24.localeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocaleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocaleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/esnack24/localeapi/LocaleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esnack24.localeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocaleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
